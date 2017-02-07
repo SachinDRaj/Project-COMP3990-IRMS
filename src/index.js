dcdcd1efc8571c47768ebf8d671b92fc08edcd5c
SachinDRaj <derycksachin@gmail.com>
2017-02-07 13:55:16 -0400

    fix some stuff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 angular
   .module('app', ['ui.router','uiGmapgoogle-maps']);
-
-
-
-
-  jQuery(document).ready(function($) {
-      $(".clickable-row").click(function() {
-          window.document.location = $(this).data("href");
-      });
-  });
