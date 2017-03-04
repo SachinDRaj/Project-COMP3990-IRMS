c41e50bab765e192a9d24394956dcccccc6a0f28
SachinDRaj <derycksachin@gmail.com>
2017-03-03 21:12:24 -0400

    add map to makereport4

--- src/routes.js
+++ src/routes.js
@@ -102,6 +102,13 @@ angular
   })
   .controller('makereport4Con', function($scope) {
     console.log('Make report4 controller');
+    $scope.map = {
+      center: {
+        latitude: 10.450429,
+        longitude: -61.314820
+      },
+      zoom: 9
+    };
     document.getElementById("ca").innerHTML = localStorage.getItem("select");
     document.getElementById("tt").innerHTML = localStorage.getItem("title");
     document.getElementById("de").innerHTML = localStorage.getItem("desc");
